fdfa24c36faa922531f58447ccd68c2dead31bf6
jiyemun <jym6853@gmail.com>
2023-06-22 09:46:46 +0900

    Design: layout add

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <template>
-  <div id="test">0</div>
+  <Layout :style="{ height: '100vh' }">
+    <LayoutHeader class="header">
+      <div class="logo" />
+      <Menu
+        v-model:selectedKeys="selectedKeys"
+        theme="dark"
+        mode="horizontal"
+        :style="{ lineHeight: '64px' }"
+      >
+        <MenuItem key="1">nav 1</MenuItem>
+        <MenuItem key="2">nav 2</MenuItem>
+        <MenuItem key="3">nav 3</MenuItem>
+      </Menu>
+    </LayoutHeader>
+    <LayoutContent>
+      <div :style="{ background: '#fff', padding: '24px', height: '100%' }">
+        Content
+      </div>
+    </LayoutContent>
+    <LayoutFooter> ©2023 Created by Jiye </LayoutFooter>
+  </Layout>
 </template>
 
 <script lang="ts" setup>
-import { observable } from "./example/ex4";
+import { ref } from "vue";
+import {
+  MenuItem,
+  Layout,
+  LayoutHeader,
+  LayoutFooter,
+  LayoutContent,
+  Menu,
+} from "ant-design-vue";
+
+const selectedKeys = ref<string[]>(["1"]);
 </script>
 
 <style scoped></style>
