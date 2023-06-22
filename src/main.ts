9131279404c447d9c927078565821455e679f67c
jiyemun <jym6853@gmail.com>
2023-06-22 09:28:39 +0900

    Style: ant design css import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import "ant-design-vue/dist/antd.css"; // or 'ant-design-vue/dist/antd.less'
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
