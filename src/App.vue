f809058781ce9cc43cae4e344089a0613ab0cc7e
jiyemun <jym6853@gmail.com>
2023-06-22 11:48:49 +0900

    Refactor: home menu 리펙토링

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,22 @@
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
       >
-        <MenuItem key="1">HOME</MenuItem>
-        <MenuItem key="2">EX22</MenuItem>
-        <MenuItem key="3">EX23</MenuItem>
+        <MenuItem
+          v-for="(item, index) in menu"
+          :key="index"
+          style="text-transform: uppercase"
+        >
+          {{ item.title }}
+        </MenuItem>
       </Menu>
     </LayoutHeader>
     <LayoutContent>
       <div :style="{ background: '#fff', padding: '24px', height: '100%' }">
-        <template v-if="selectedKeys == '1'">
-          <Home />
-        </template>
-        <template v-if="selectedKeys == '2'">
-          <Example22 />
+        <template v-for="(item, index) in menu">
+          <component
+            :is="item.component"
+            v-if="selectedKeys == index"
+          ></component>
         </template>
       </div>
     </LayoutContent>
@@ -40,7 +44,18 @@ import {
 import Home from "./components/Home.vue";
 import Example22 from "./components/Example22.vue";
 
-const selectedKeys = ref<string[]>(["1"]);
+const selectedKeys = ref<number[]>([0]);
+
+const menu = [
+  {
+    title: "home",
+    component: Home,
+  },
+  {
+    title: "ex22",
+    component: Example22,
+  },
+];
 </script>
 
 <style scoped></style>
